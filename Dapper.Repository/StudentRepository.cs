867c7aae45aef2c5b5bb1089de3b8db9e5120991
Columbia91 <alpha.z91@mail.ru>
2019-04-29 21:51:45 +0600

    Добавьте файлы проекта.

---
+++ Dapper.Repository/StudentRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Models;
+
+namespace Dapper.Repository
+{
+    public class StudentRepository
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\АхметкалиА\source\repos\DapperLesson\Dapper.Repository\StudentsDB.mdf;Integrated Security=True";
+
+        public string InsertStudent(string query, Student student)
+        {
+            using(var sql = new SqlConnection(connectionString))
+            {
+                var result = sql.Execute(query, student);
+                if (result < 1)
+                    throw new Exception("Ошибка при вставке записи");
+            }
+            return "Вставка произошла успешно";
+        }
+
+        public List<Student> GetAllStudent(string query)
+        {
+            using(var sql = new SqlConnection(connectionString))
+            {
+                return sql.Query<Student>(query).ToList();
+            }
+        }
+
+        public int GetGroupId(string name)
+        {
+            using(var sql = new SqlConnection(connectionString))
+            {
+                return sql.Query<int>(@"Select id from Groups Wher Name = ")
+            }
+
+            return db.Query<User>("SELECT * FROM Users WHERE Id = @id", new { id }).FirstOrDefault();
+        }
+    }
+}
---
+++ DapperConsoleApp/Program.cs
@@ -0,0 +1,80 @@
+﻿using Dapper.Models;
+using Dapper.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StudentRepository _userRep = new StudentRepository();
+            //do
+            //{
+            //    _userRep.InsertStudent(@"INSERT INTO Students (FirstName, LastName, MiddleName, GroupId) values
+            //                            (@FirstName, @LastName, @MiddleName, @GroupId)",
+            //                            new Student()
+            //                            {
+            //                                LastName = "Rudenko",
+            //                                FirstName = "Sergey",
+            //                                MiddleName = "Dmitrievich",
+            //                                GroupId = 1
+            //                            });
+
+            //    var students = _userRep.GetAllStudent("Select * from Students");
+            //    students.ForEach(f =>
+            //    {
+            //        Console.WriteLine($"{f.Id}\t{f.LastName}\t{f.FirstName}\t{f.MiddleName}");
+            //    });
+            //} while (Console.ReadKey().Key == ConsoleKey.Escape);
+
+            do
+            {
+                int choice = 0;
+                bool check = false;
+                while (!check)
+                {
+                    Console.Clear();
+                    Console.Write("\tБаза данных студентов\n" +
+                        "1) Добавить данные\n" +
+                        "2) Обновить данные\n" +
+                        "3) Удалить данные\n\n" +
+                        "4) Посмотреть данные\n" +
+                        "Ваш выбор: ");
+                    check = int.TryParse(Console.ReadLine(), out choice);
+                    if (choice < 1 || choice > 4)
+                        check = false;
+                }
+
+                if(choice == 1)
+                {
+                    StudentRepository repository = new StudentRepository();
+
+                    string[] properties = { "Имя: ", "Фамилия: ", "Отчество: ", "Номер группы: " };
+                    string[] data = new string[properties.Length];
+                    int groupId = 0;
+
+                    Console.WriteLine("Введите данные о студенте");
+                    for (int i = 0; i < properties.Length; i++)
+                    {
+                        Console.Write(properties[i]);
+                        data[i] = Console.ReadLine();
+                        if (properties[i] == "Номер группы")
+                            groupId = repository.GetGroupId(data[i]);
+                    }
+
+                    int counter = 0;
+                    Student student = new Student();
+                    student.FirstName = data[counter++];
+                    student.LastName = data[counter++];
+                    student.MiddleName = data[counter];
+                    student.GroupId = groupId;
+                }
+            } while (Console.ReadKey().Key == ConsoleKey.Escape);
+        }
+    }
+}
